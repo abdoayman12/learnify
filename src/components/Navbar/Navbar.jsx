882bc41f7055471e5ddec5f8a34676ea767da6d7
abdullah <kmylbdallh13@gmail.com>
2026-03-12 13:02:00 +0200

    Remove unused import of useRef from Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { NavLink, Link, useLocation } from "react-router-dom";
 import {
     Search,
