0eb7377ab83fd5deb2221655105f61ce41f3df3a
mazin189 <mazen.ahmed.abdelattii@gmail.com>
2026-03-13 18:45:54 +0200

    home page updates

--- src/components/Navbar/Home.jsx
+++ src/components/Navbar/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Check, MoveRight , Star , ShieldCheck , Clock4, CodeXml ,BarChart3,PenTool,Briefcase,Megaphone,Smartphone,Cloud,Camera } from 'lucide-react';
-
-
-
-
+import { Check, MoveRight , Star , ShieldCheck , Clock4, CodeXml ,BarChart3,
+PenTool,Briefcase,Megaphone,Smartphone,Cloud,Camera, 
+Play} from 'lucide-react';
+import laptopImg from "../../assets/laptop.jpg"
+import girl1 from "../../assets/girl1.jpg"
+import girl2 from "../../assets/girl2.jpg"
+import boy1 from "../../assets/boy1.jpg"
+import boy2 from "../../assets/boy2.jpg"
+import codeImg from "../../assets/code.jpg"
+import planetImg from "../../assets/planet.jpg"
+import uxDesignImg from "../../assets/uxdesign.jpg"
 
 function Home() {
 
@@ -17,7 +23,7 @@ function Home() {
  
 <div className='relative text-center py-24 h-screen bg-gradient-to-br from-blue-950 via-indigo-900 to-blue-950'>
 
-<div className="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.05)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.05)_1px,transparent_1px)] bg-[size:44px_44px]"></div>
+<div className="absolute pointer-events-none inset-0 bg-[linear-gradient(rgba(255,255,255,0.05)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.05)_1px,transparent_1px)] bg-[size:44px_44px]"></div>
 
 <div className='items-center gap-1.5 border border-primary-400/30 bg-primary-500/10 inline-flex rounded-full py-2 px-4 mb-6'>
 <div className='rounded-full w-4 h-4 bg-primary-300 flex items-center justify-center'><Check size={10}/></div>
@@ -26,30 +32,30 @@ function Home() {
 
 
 
-<p className='text-6xl font-extrabold tracking-tighter text-white'>Learn Without Limits</p>
-<p className='text-6xl font-extrabold tracking-tighter text-cyan-600 mt-1 mb-4'>Learnify</p>
+<p className='text-4xl md:text-6xl font-extrabold tracking-tighter text-white'>Learn Without Limits</p>
+<p className='text-4xl md:text-6xl font-extrabold tracking-tighter text-cyan-600 mt-1 mb-4'>Learnify</p>
 
 
 
 
 <div className='font-medium text-xl text-primary-200 mb-8'>
-<span>Master new skills with world-class instructors. Access thousands of courses in</span>
-<span className='block'>technology, business, design, and more.</span>
+<span>Master new skills with world-class instructors. Access thousands of courses in </span>
+<span className='inline md:block'>technology, business, design, and more.</span>
 </div>
 
 
 
 
-<div className='flex gap-4 items-center justify-center mb-9'>
+<div className='flex flex-col md:flex-row gap-4 items-center justify-center mb-9 px-4 md:px-0'>
 
-<div className='hover:shadow-md bg-white rounded-full py-4 px-7 inline-flex items-center text-primary-600 gap-2 cursor-pointer'>
+<div className='hover:shadow-2xl bg-white rounded-full py-4 px-7 inline-flex items-center justify-center text-primary-600 gap-2 cursor-pointer w-full md:w-fit'>
 <button className='font-medium text-[19px]'>Explore Courses</button>
 <MoveRight size={18}/>
 </div>
 
 
 
-<div className='border-2 hover:border-white hover:bg-white/10 border-white/30 rounded-full py-4 px-7 inline-flex text-white cursor-pointer'>
+<div className='border-2 hover:border-white hover:bg-white/10 w-full md:w-fit border-white/30 rounded-full py-4 px-7 items-center justify-center inline-flex text-white cursor-pointer'>
 <button className='font-medium text-xl'>View Pricing</button>
 </div>
 
@@ -60,7 +66,7 @@ function Home() {
 
 
 
-<div className='flex items-center gap-7 justify-center mb-14'>
+<div className='flex flex-wrap items-center gap-5 md:gap-7 justify-center mb-14 px-5 md:px-0'>
 
 
 
@@ -71,7 +77,7 @@ function Home() {
 
 
 
-<div className='flex items-center gap-2'>
+<div className='flex items-center gap-0 md:gap-2'>
 <ShieldCheck className='text-green-500'/>     
 <p className='text-primary-200'>30-day money-back guarantee</p>
 </div>   
@@ -92,21 +98,21 @@ function Home() {
 
 
 
-<div className='container max-w-5xl mx-auto w-full flex items-center justify-between'>
+<div className='container max-w-5xl mx-auto w-full grid grid-cols-2 gap-7 md:gap-0 md:flex items-center justify-between'>
 
 
 
 
 <div>
-<p className='text-white font-extrabold text-5xl'>50K+</p>
-<p className='text-primary-300 text-[18px]'>Active Students</p>
+<p className='text-white font-extrabold text-4xl md:text-5xl'>50K+</p>
+<p className='text-primary-300 text-[18px] mt-1'>Active Students</p>
 </div>
 
 
 
 <div>
-<p className='text-white font-extrabold text-5xl'>500+</p>
-<p className='text-primary-300 text-[18px]'>Expert Instructors</p>
+<p className='text-white font-extrabold text-4xl md:text-5xl'>500+</p>
+<p className='text-primary-300 text-[18px] mt-1'>Expert Instructors</p>
 </div>
 
 
@@ -116,15 +122,15 @@ function Home() {
 
 
 <div>
-<p className='text-white font-extrabold text-5xl'>2,500+</p>
-<p className='text-primary-300 text-[18px]'>Quality Courses</p>
+<p className='text-white font-extrabold text-4xl md:text-5xl'>2,500+</p>
+<p className='text-primary-300 text-[18px] mt-1'>Quality Courses</p>
 </div>
 
 
 
 <div>
-<p className='text-white font-extrabold text-5xl'>95%</p>
-<p className='text-primary-300 text-[18px]'>Success Rate</p>
+<p className='text-white font-extrabold text-4xl md:text-5xl'>95%</p>
+<p className='text-primary-300 text-[18px] mt-1'>Success Rate</p>
 </div>
 
 
@@ -148,9 +154,9 @@ function Home() {
 
 
 
-<div className='text-center'>
+<div className='text-center mx-4 md:mx-0'>
   <p className='uppercase text-primary-600 font-semibold tracking-wider text-sm'>Browse Categories</p>
-<p className='text-white font-bold text-4xl pb-2 pt-3'>Explore Our Top Categories</p>
+<p className='text-white font-bold text-3xl md:text-4xl pb-2 pt-3'>Explore Our Top Categories</p>
 <p className='text-gray-400 font-medium pb-12'>Find the perfect course from our wide selection of categories taught by industry experts.</p>
 
 </div>
@@ -168,7 +174,7 @@ function Home() {
 
 
 <div className='max-w-[1290px] mx-auto container w-full'>
-<div className='grid grid-cols-4 gap-6'>
+<div className=' md:grid md:grid-cols-4 flex flex-col gap-6 mx-4 md:mx-0'>
 
 
 <div className='bg-gray-900 w-full ps-7 py-7 rounded-2xl group border border-gray-800 hover:border-primary-800 cursor-pointer'>
@@ -335,11 +341,639 @@ function Home() {
 
 
 
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div className='bg-gray-900 py-28'>
+
+<div className='max-w-[1290px] mx-auto container w-full'>
+
+
+
+
+
+
+<div className='mx-4 md:mx-0'>
+ 
+
+<div className='flex items-center justify-between'>
+
+
+<div>
+<p className='uppercase text-primary-600 font-semibold tracking-wider text-sm'>Featured Courses</p>
+<p className='text-white font-bold text-3xl md:text-4xl py-2.5'>Most Popular Courses</p>
+<span className='text-gray-400 font-medium'>Start learning from the highest-rated courses chosen by thousands of students </span>
+<span className='text-gray-400 font-medium pb-12 inline md:block leading-none'>worldwide.</span>
+</div>
+
+
+
+
+<div className='items-center gap-2 text-primary-400 hover:text-indigo-800 inline-flex cursor-pointer'>
+<span className='font-semibold'>View All Courses</span>
+<MoveRight size={18}/>
+</div>
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div className='flex items-center gap-6'>
+<div className='rounded-2xl overflow-hidden border border-gray-800 bg-slate-900 cursor-pointer group hover:-translate-y-1 transition-all duration-300'>
+
+
+
+
+
+
+
+<div className='relative '>
+<img src={laptopImg} alt="laptop" className='w-full group-hover:scale-[1.03] transition-all duration-300'/>
+<div className='absolute inset-0 '>
+<div className='inline-flex flex-col gap-2 p-3'>
+<div className='uppercase bg-amber-500 text-white text-xs rounded-full font-extrabold inline-flex justify-center items-center px-2.5 py-1 tracking-tight'>
+BestSeller
+</div>
+<div className='uppercase bg-red-500 text-white text-xs rounded-full font-extrabold inline-flex justify-center items-center ps-2.5 pe-7 py-1 tracking-tight'>
+55% OFF
+</div>
+</div>
+</div>
+<div className='absolute bottom-3 right-3 text-white text-xs rounded-full font-extrabold bg-slate-800 inline-flex justify-center items-center px-2.5 py-1 tracking-tight'>
+65 hours
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div className='px-5 pt-5'>
+
+<p className='uppercase text-indigo-400 text-sm font-semibold'>Web Development</p>
+
+
+<div className='text-white font-semibold text-xl pt-3 pb-4 group-hover:text-indigo-400'>
+<span className=''>The Complete Web </span>
+<span className='inline-block md:block leading-none '>Development Bootcamp...</span>
+</div>
+
+
+
+<div className='flex items-center gap-2 pb-2'>
+<img src={girl1} alt="girl" className='rounded-full w-6 h-6'/>
+<span className='text-gray-400 font-medium text-sm'>Sarah Johnson</span>
+</div>
+
+
+
+
+
+
+
+<div className='flex items-center gap-4 pb-2'>
+<div className='flex items-center gap-1'>
+<Star stroke='#eab308' fill='#eab308' size={14}/>    
+<div className='flex items-center gap-1'>
+<span className='text-white font-bold'>4.8</span>
+<span className='text-gray-500'>(46K)</span>
+</div>
+</div>
+<span className='text-gray-500'>235K students</span>
+</div>
+
+
+
+
+
+
+
+<div className='flex items-center gap-4'>
+<div className='flex items-center gap-1 text-gray-400'>
+<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-no-axes-column-increasing-icon lucide-chart-no-axes-column-increasing"><path d="M5 21v-6"/><path d="M12 21V9"/><path d="M19 21V3"/></svg>
+<span className='text-[15px] font-medium'>All Levels</span>
+</div>
+
+
+
+
+<div className='flex items-center gap-1 text-gray-400'>
+<Play size={12}/>
+<span className='text-[15px] font-medium'>478 lessons</span>
+</div>
+</div>
+
+
+
+<div className='border-t border-gray-800 py-3 flex items-center gap-2 mt-4'>
+
+<span className='text-white font-bold text-[22px]'>$89.99</span>
+<span className='text-gray-600'><del>$199.99</del></span>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 </div>
 
+
+
+</div>
+
+
+
+
+<div className='rounded-2xl overflow-hidden border border-gray-800 bg-slate-900 cursor-pointer group hover:-translate-y-2 transition-all duration-300'>
+
+<div className='relative '>
+<img src={codeImg} alt="laptop" className='w-full group-hover:scale-[1.03] transition-all duration-300'/>
+<div className='absolute inset-0 '>
+<div className='inline-flex flex-col gap-2 p-3'>
+<div className='uppercase bg-amber-500 text-white text-xs rounded-full font-extrabold inline-flex justify-center items-center px-2.5 py-1 tracking-tight'>
+BestSeller
+</div>
+<div className='uppercase bg-red-500 text-white text-xs rounded-full font-extrabold inline-flex justify-center items-center ps-2.5 pe-7 py-1 tracking-tight'>
+50% OFF
+</div>
+</div>
+</div>
+<div className='absolute bottom-3 right-3 text-white text-xs rounded-full font-extrabold bg-slate-800 inline-flex justify-center items-center px-2.5 py-1 tracking-tight'>
+44 hours
+</div>
+</div>
+
+
+<div className='px-5 pt-5'>
+
+<p className='uppercase text-indigo-400 text-sm font-semibold'>data science</p>
+
+
+<div className='text-white font-semibold text-xl pt-3 pb-4 group-hover:text-indigo-400'>
+<span className=''>Machine Learning A-Z: AI, </span>
+<span className='inline-block md:block leading-none '>Python & R</span>
+</div>
+
+
+
+<div className='flex items-center gap-2 pb-2'>
+<img src={boy1} alt="girl" className='rounded-full w-6 h-6'/>
+<span className='text-gray-400 font-medium text-sm'>Michael Chen</span>
+</div>
+
+
+
+
+
+
+
+<div className='flex items-center gap-4 pb-2'>
+<div className='flex items-center gap-1'>
+<Star stroke='#eab308' fill='#eab308' size={14}/>    
+<div className='flex items-center gap-1'>
+<span className='text-white font-bold'>4.7</span>
+<span className='text-gray-500'>(32K)</span>
+</div>
+</div>
+<span className='text-gray-500'>178K students</span>
+</div>
+
+
+
+
+
+
+
+<div className='flex items-center gap-4'>
+<div className='flex items-center gap-1 text-gray-400'>
+<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-no-axes-column-increasing-icon lucide-chart-no-axes-column-increasing"><path d="M5 21v-6"/><path d="M12 21V9"/><path d="M19 21V3"/></svg>
+<span className='text-[15px] font-medium'>Intermediate</span>
+</div>
+
+
+
+
+<div className='flex items-center gap-1 text-gray-400'>
+<Play size={12}/>
+<span className='text-[15px] font-medium'>312 lessons</span>
+</div>
+</div>
+
+
+
+<div className='border-t border-gray-800 py-3 flex items-center gap-2 mt-4'>
+<span className='text-white font-bold text-[22px]'>$94.99</span>
+<span className='text-gray-600'><del>$189.99</del></span>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+</div>
+
+
+
+
+<div className='rounded-2xl overflow-hidden border border-gray-800 bg-slate-900 cursor-pointer group hover:-translate-y-2 transition-all duration-300'>
+
+
+<div className='relative '>
+<img src={uxDesignImg} alt="laptop" className='w-full group-hover:scale-[1.03] transition-all duration-300'/>
+<div className='absolute inset-0 '>
+<div className='inline-flex flex-col gap-2 p-3'>
+<div className='uppercase bg-green-500 text-white text-xs rounded-full font-extrabold inline-flex justify-center items-center py-1 tracking-tight'>
+NEW
+</div>
+<div className='uppercase bg-red-500 text-white text-xs rounded-full font-extrabold inline-flex justify-center items-center ps-2.5 pe-7 py-1 tracking-tight'>
+50% OFF
+</div>
+</div>
+</div>
+<div className='absolute bottom-3 right-3 text-white text-xs rounded-full font-extrabold bg-slate-800 inline-flex justify-center items-center px-2.5 py-1 tracking-tight'>
+38 hours
+</div>
+</div>
+
+
+<div className='px-5 pt-5'>
+
+<p className='uppercase text-indigo-400 text-sm font-semibold'>UI/UX Design</p>
+
+
+<div className='text-white font-semibold text-xl pt-3 pb-4 group-hover:text-indigo-400'>
+<span className=''>UI/UX Design: The </span>
+<span className='inline-block md:block leading-none '>Complete Guide</span>
+</div>
+
+
+
+<div className='flex items-center gap-2 pb-2'>
+<img src={girl2} alt="girl" className='rounded-full w-6 h-6'/>
+<span className='text-gray-400 font-medium text-sm'>Emily Rodriguez</span>
+</div>
+
+
+
+
+
+
+
+<div className='flex items-center gap-4 pb-2'>
+<div className='flex items-center gap-1'>
+<Star stroke='#eab308' fill='#eab308' size={14}/>    
+<div className='flex items-center gap-1'>
+<span className='text-white font-bold'>4.9</span>
+<span className='text-gray-500'>(19K)</span>
+</div>
+</div>
+<span className='text-gray-500'>89K students</span>
+</div>
+
+
+
+
+
+
+
+<div className='flex items-center gap-4'>
+<div className='flex items-center gap-1 text-gray-400'>
+<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-no-axes-column-increasing-icon lucide-chart-no-axes-column-increasing"><path d="M5 21v-6"/><path d="M12 21V9"/><path d="M19 21V3"/></svg>
+<span className='text-[15px] font-medium'>Beginner</span>
+</div>
+
+
+
+
+<div className='flex items-center gap-1 text-gray-400'>
+<Play size={12}/>
+<span className='text-[15px] font-medium'>245 lessons</span>
+</div>
+</div>
+
+
+
+<div className='border-t border-gray-800 py-3 flex items-center gap-2 mt-4'>
+
+<span className='text-white font-bold text-[22px]'>$79.99</span>
+<span className='text-gray-600'><del>$159.99</del></span>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+</div>
+
+
+
+
+<div className='rounded-2xl overflow-hidden border border-gray-800 bg-slate-900 cursor-pointer group hover:-translate-y-2 transition-all duration-300'>
+
+
+<div className='relative '>
+<img src={planetImg} alt="laptop" className='w-full group-hover:scale-[1.03] transition-all duration-300'/>
+<div className='absolute inset-0 '>
+<div className='inline-flex flex-col gap-2 p-3'>
+<div className='uppercase bg-amber-500 text-white text-xs rounded-full font-extrabold inline-flex justify-center items-center px-2.5 py-1 tracking-tight'>
+BestSeller
+</div>
+<div className='uppercase bg-red-500 text-white text-xs rounded-full font-extrabold inline-flex justify-center items-center ps-2.5 pe-7 py-1 tracking-tight'>
+50% OFF
+</div>
+</div>
+</div>
+<div className='absolute bottom-3 right-3 text-white text-xs rounded-full font-extrabold bg-slate-800 inline-flex justify-center items-center px-2.5 py-1 tracking-tight'>
+52 hours
+</div>
+</div>
+
+
+ 
+
+<div className='px-5 pt-5'>
+
+<p className='uppercase text-indigo-400 text-sm font-semibold'>Cloud & DevOps</p>
+
+
+<div className='text-white font-semibold text-xl pt-3 pb-4 group-hover:text-indigo-400'>
+<span className=''>AWS Certified Solutions </span>
+<span className='inline-block md:block leading-none '>Architect 2032</span>
+</div>
+
+
+
+<div className='flex items-center gap-2 pb-2'>
+<img src={boy2} alt="girl" className='rounded-full w-6 h-6'/>
+<span className='text-gray-400 font-medium text-sm'>David Kim</span>
+</div>
+
+
+
+
+
+
+
+<div className='flex items-center gap-4 pb-2'>
+<div className='flex items-center gap-1'>
+<Star stroke='#eab308' fill='#eab308' size={14}/>    
+<div className='flex items-center gap-1'>
+<span className='text-white font-bold'>4.8</span>
+<span className='text-gray-500'>(25K)</span>
+</div>
+</div>
+<span className='text-gray-500'>157K students</span>
+</div>
+
+
+
+
+
+
+
+<div className='flex items-center gap-4'>
+<div className='flex items-center gap-1 text-gray-400'>
+<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-no-axes-column-increasing-icon lucide-chart-no-axes-column-increasing"><path d="M5 21v-6"/><path d="M12 21V9"/><path d="M19 21V3"/></svg>
+<span className='text-[15px] font-medium'>Intermediate</span>
+</div>
+
+
+
+
+<div className='flex items-center gap-1 text-gray-400'>
+<Play size={12}/>
+<span className='text-[15px] font-medium'>380 lessons</span>
+</div>
+</div>
+
+
+
+<div className='border-t border-gray-800 py-3 flex items-center gap-2 mt-4'>
+
+<span className='text-white font-bold text-[22px]'>$84.99</span>
+<span className='text-gray-600'><del>$169.99</del></span>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</div>
 
 
 
